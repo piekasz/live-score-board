dc611578329b6521c0c8545216c3b3e8627a535f
Piotr Piekarski <piotrek199507@gmail.com>
2025-06-27 18:27:35 +0200

    refactor tests - add some const values

--- src/test/groovy/pl/ppiekarski/livescoreboard/LiveScoreBoardSpec.groovy
+++ src/test/groovy/pl/ppiekarski/livescoreboard/LiveScoreBoardSpec.groovy
@@ -4,50 +4,53 @@ import spock.lang.Specification
 
 class LiveScoreBoardSpec extends Specification {
 
+    private static final String HOME_TEAM_NAME = "teamA"
+    private static final String AWAY_TEAM_NAME = "teamB"
+    private static final startMatchCommand1 = new StartMatchCommand(HOME_TEAM_NAME, AWAY_TEAM_NAME)
+    public static final String OTHER_HOME_TEAM_NAME = "teamC"
+    public static final String OTHER_AWAY_TEAM_NAME = "teamD"
+    public static final startMatchCommand2 = new StartMatchCommand(OTHER_HOME_TEAM_NAME, OTHER_AWAY_TEAM_NAME)
+
     def liveScoreBoard = new LiveScoreBoard()
 
     def "should start a match and store it in liveScoreBoard"() {
         when:
-        def result = liveScoreBoard.startMatch(new StartMatchCommand("teamA", "teamB"))
+        def result = liveScoreBoard.startMatch(startMatchCommand1)
         then: "started match is in liveScoreBoard"
-        liveScoreBoard.getSummary() == [new MatchDto("teamA", "teamB", 0, 0)]
-        and: "matchId is returned as result"
+        liveScoreBoard.getSummary() == [new MatchDto(HOME_TEAM_NAME, AWAY_TEAM_NAME, 0, 0)]
+        and: "matchId is returned in success result"
         result.isSuccess()
         result.getOrNull() instanceof MatchId
     }
 
     def "should update score correctly"() {
         given:
-        MatchId matchId = liveScoreBoard.startMatch(new StartMatchCommand("teamA", "teamB"))
-                .getOrNull()
+        MatchId matchId = liveScoreBoard.startMatch(startMatchCommand1).getOrNull()
         when:
         liveScoreBoard.updateScore(new UpdateScoreCommand(matchId, 1, 0))
         liveScoreBoard.updateScore(new UpdateScoreCommand(matchId, 1, 1))
         liveScoreBoard.updateScore(new UpdateScoreCommand(matchId, 1, 2))
         then: "score is updated in liveScoreBoard"
-        liveScoreBoard.getSummary() == [new MatchDto("teamA", "teamB", 1, 2)]
+        liveScoreBoard.getSummary() == [new MatchDto(HOME_TEAM_NAME, AWAY_TEAM_NAME, 1, 2)]
     }
 
-    def "should update score for proper game"() {
+    def "should update score for proper match"() {
         given:
-        MatchId matchId1 = liveScoreBoard.startMatch(new StartMatchCommand("teamA", "teamB"))
-                .getOrNull()
-        MatchId matchId2 = liveScoreBoard.startMatch(new StartMatchCommand("teamC", "teamD"))
-                .getOrNull()
+        MatchId matchId1 = liveScoreBoard.startMatch(startMatchCommand1).getOrNull()
+        MatchId matchId2 = liveScoreBoard.startMatch(startMatchCommand2).getOrNull()
         when:
         liveScoreBoard.updateScore(new UpdateScoreCommand(matchId1, 1, 0))
         liveScoreBoard.updateScore(new UpdateScoreCommand(matchId2, 2, 2))
         then: "score is updated in liveScoreBoard"
         liveScoreBoard.getSummary() as Set == [
-                new MatchDto("teamC", "teamD", 2, 2),
-                new MatchDto("teamA", "teamB", 1, 0),
+                new MatchDto(OTHER_HOME_TEAM_NAME, OTHER_AWAY_TEAM_NAME, 2, 2),
+                new MatchDto(HOME_TEAM_NAME, AWAY_TEAM_NAME, 1, 0),
         ] as Set
     }
 
     def "should not update when score is invalid"() {
         given:
-        MatchId matchId = liveScoreBoard.startMatch(new StartMatchCommand("teamA", "teamB"))
-                .getOrNull()
+        MatchId matchId = liveScoreBoard.startMatch(startMatchCommand1).getOrNull()
         when:
         def result = liveScoreBoard.updateScore(new UpdateScoreCommand(matchId, 0, -1))
         then:

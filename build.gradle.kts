c94ca2fda78809a169d67d402a26407380708268
Piotr Piekarski <piotrek199507@gmail.com>
2025-06-27 17:17:55 +0200

    gradle - add spock

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("java")
+    java
+    groovy
 }
 
 group = "pl.ppiekarski"
@@ -9,7 +10,14 @@ repositories {
     mavenCentral()
 }
 
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
 dependencies {
+
+    testImplementation("org.apache.groovy:groovy:4.0.21")
+    testImplementation("org.spockframework:spock-core:2.3-groovy-4.0")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }

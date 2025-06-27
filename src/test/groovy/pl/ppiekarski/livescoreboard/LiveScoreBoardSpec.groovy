d4931b24da7330221986285d1ed15e71b5e6deab
Piotr Piekarski <piotrek199507@gmail.com>
2025-06-27 17:56:20 +0200

    add "should update score correctly" test

--- src/test/groovy/pl/ppiekarski/livescoreboard/LiveScoreBoardSpec.groovy
+++ src/test/groovy/pl/ppiekarski/livescoreboard/LiveScoreBoardSpec.groovy
@@ -6,10 +6,21 @@ class LiveScoreBoardSpec extends Specification {
 
     def liveScoreBoard = new LiveScoreBoard()
 
-    def "should start a match and store it in liveScoreBoard"(){
+    def "should start a match and store it in liveScoreBoard"() {
         when:
         liveScoreBoard.startMatch("teamA", "teamB")
         then: "started match is in liveScoreBoard"
         liveScoreBoard.getSummary() == [new MatchDto("teamA", "teamB")]
+    }
+
+    def "should update score correctly"() {
+        given:
+        MatchId matchId = liveScoreBoard.startMatch("teamA", "teamB")
+        when:
+        liveScoreBoard.updateScore(matchId, 1, 0)
+        liveScoreBoard.updateScore(matchId, 1, 1)
+        liveScoreBoard.updateScore(matchId, 1, 2)
+        then: "score is updated in liveScoreBoard"
+        liveScoreBoard.getSummary() == [new MatchDto("teamA", "teamB", 1, 2)]
     }
 }

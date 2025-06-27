01e3c9d35ac716473f949bdaf74408a67542c366
Piotr Piekarski <piotrek199507@gmail.com>
2025-06-27 17:29:29 +0200

    add red test - "should start a match"

---
+++ src/test/groovy/pl/ppiekarski/livescoreboard/LiveScoreBoardSpec.groovy
@@ -0,0 +1,15 @@
+package pl.ppiekarski.livescoreboard
+
+import spock.lang.Specification
+
+class LiveScoreBoardSpec extends Specification {
+
+    def liveScoreBoard = new LiveScoreBoard()
+
+    def "should start a match and store it in liveScoreBoard"(){
+        when:
+        liveScoreBoard.startMatch("teamA", "teamB")
+        then: "started match is in liveScoreBoard"
+        liveScoreBoard.getSummary().containsMatch("teamA", "teamB")
+    }
+}
